828e2ea4f54fd62e9f437ed87c35e2545791d2f5
Robert Warren <grindnmosh@gmail.com>
2015-01-06 13:42:01 -0700

    Sign In and Sign Up is complete Stays signed in. Sign out not implemented yet

--- Friends/app/src/main/java/com/grinddesign/friends/LoginActivity.java
+++ Friends/app/src/main/java/com/grinddesign/friends/LoginActivity.java
@@ -1,29 +1,119 @@
 package com.grinddesign.friends;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
-import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
-public class LoginActivity extends ActionBarActivity {
+
+public class LoginActivity extends Activity {
+
+    Button signin;
+    Button signup;
+    String userid;
+    String pass;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
 
-        Parse.initialize(this, "jR7Hl4PT0g2y5BZQP1kpwHduKbSKs5B8ZqUZgkW7", "T3wn1MrWL1cQb3Act2u3GvSFlkJZIegx55gdmdqy");
 
         //Test successful
         /*ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
         testObject.saveInBackground();*/
+
+
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        signin = (Button) findViewById(R.id.sibutt);
+        signup = (Button) findViewById(R.id.subutt);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userid = username.getText().toString();
+                pass = password.getText().toString();
+
+                ParseUser.logInInBackground(userid, pass,
+                        new LogInCallback() {
+                            public void done(ParseUser parseUser, com.parse.ParseException e) {
+                                if (parseUser != null) {
+                                    // If user exist and authenticated, send user to FriendListActivity.class
+                                    Intent intent = new Intent(
+                                            LoginActivity.this,
+                                            FriendListActivity.class);
+                                    startActivity(intent);
+                                    Toast.makeText(getApplicationContext(),
+                                            "Successfully Logged in",
+                                            Toast.LENGTH_LONG).show();
+                                    finish();
+                                } else {
+                                    Toast.makeText(
+                                            getApplicationContext(),
+                                            "No such user exist, please signup",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                        });
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Retrieve the text entered from the EditText
+                userid = username.getText().toString();
+                pass = password.getText().toString();
+
+                // Force user to fill up the form
+                if (userid.equals("") && pass.equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please complete the sign up form",
+                            Toast.LENGTH_LONG).show();
+
+                } else {
+                    // Save new user data into Parse.com Data Storage
+                    ParseUser user = new ParseUser();
+                    user.setUsername(userid);
+                    user.setPassword(pass);
+                    user.signUpInBackground(new SignUpCallback() {
+                        public void done(com.parse.ParseException e) {
+                            if (e == null) {
+                                // Show a simple Toast message upon successful registration
+                                Toast.makeText(getApplicationContext(),
+                                        "Successfully Signed up, please log in.",
+                                        Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Sign up Error", Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+
+
+                    });
+                }
+
+            }
+        });
     }
 
 
---
+++ Friends/app/src/main/java/com/grinddesign/friends/MainActivity.java
@@ -0,0 +1,53 @@
+package com.grinddesign.friends;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
+
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "jR7Hl4PT0g2y5BZQP1kpwHduKbSKs5B8ZqUZgkW7", "T3wn1MrWL1cQb3Act2u3GvSFlkJZIegx55gdmdqy");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+
+        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+            Intent intent = new Intent(MainActivity.this,
+                    LoginActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            if (currentUser != null) {
+                Intent intent = new Intent(MainActivity.this, FriendListActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Intent intent = new Intent(MainActivity.this,
+                        LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
+
+}
