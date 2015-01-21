2289579dacd898d38d11a53f2350369442594597
Robert Warren <grindnmosh@gmail.com>
2015-01-20 22:43:00 -0700

    update was already complete. Connection checks are in place throughout the app using my custom Jar Library PArse Local storage working fully throughout app including save eventually and delete eventually. Prevent Invalid data is mostly done from week one code. Need to implement check for data entered and limit range of numbers to be entered still needs to be done. NEW - Code to Poll Data is working and cancels for the delete feature and resumes.

---
+++ Friends/app/src/main/java/com/grinddesign/friends/NumInputFilter.java
@@ -0,0 +1,38 @@
+package com.grinddesign.friends;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+/**
+ * Author:  Robert Warren
+ * <p/>
+ * Project:  MDF3
+ * <p/>
+ * Package: com.grinddesign.friends
+ * <p/>
+ * Purpose:
+ */
+public class NumInputFilter implements InputFilter {
+    private int min;
+    private int max;
+
+    public NumInputFilter(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        //noinspection EmptyCatchBlock
+        try {
+            int input = Integer.parseInt(dest.subSequence(0, dstart).toString() + source + dest.subSequence(dend, dest.length()));
+            if (isInRange(min, max, input))
+                return null;
+        } catch (NumberFormatException nfe) { }
+        return "";
+    }
+
+    private boolean isInRange(int a, int b, int c) {
+        return b > a ? c >= a && c <= b : c >= b && c <= a;
+    }
+}
